44712caf47675b2bff4f40bde2169375c63a5abc
Albert Visser <albert@lemoncurry>
2015-09-15 23:05:42 +0200

    added the other variants

--- start_editor.py
+++ start_editor.py
@@ -1,7 +1,12 @@
 import sys
 from editor.cssedit_qt import main
-if len(sys.argv) != 1:
+if len(sys.argv) == 2:
     main(filename=sys.argv[1])
+elif len(sys.argv) == 3:
+    if sys.argv[1] == "":
+        main(text=sys.argv[2])
+    else:
+        main(tag=sys.argv[1], text=sys.argv[2])
 else:
-    main()
+    main(sys.argv)
 
